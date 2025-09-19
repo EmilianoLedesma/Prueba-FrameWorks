fabfde9cb3992b7e22b5415d1dbf990fd9ddb926
Emiliano Ledesma <emilianoledesmaledesma@outlook.com>
2025-09-19 10:55:38 -0600

    Add 'use client' directive to Footer and Gallery
    Added the 'use client' directive to Footer.tsx and Gallery.tsx to ensure these components are treated as client components in Next.js. This change is necessary for proper client-side rendering and interactivity.

--- astronomy-landing/src/components/sections/Footer.tsx
+++ astronomy-landing/src/components/sections/Footer.tsx
@@ -1,3 +1,5 @@
+'use client'
+
 import React, { useState } from 'react'
 import Container from '../ui/Container'
 import Button from '../ui/Button'
--- astronomy-landing/src/components/sections/Gallery.tsx
+++ astronomy-landing/src/components/sections/Gallery.tsx
@@ -1,3 +1,5 @@
+'use client'
+
 import React, { useState } from 'react'
 import Container from '../ui/Container'
 import { GalleryImage } from '@/types'
